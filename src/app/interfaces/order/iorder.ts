6c684e8b0c367859e1d515d51dc8b64a853dc5fd
DanielInf04 <danielinf0417@gmail.com>
2025-06-08 16:57:38 +0200

    Proyecto Final Cliente - MarketEase VFinalDefensa

--- src/app/interfaces/order/iorder.ts
+++ src/app/interfaces/order/iorder.ts
@@ -8,6 +8,17 @@ export interface PedidoEmpresa {
         cliente_id: number;
         fecha_pedido: string;
         nombre_completo: string;
+         direccion_entrega: {
+            calle: string;
+            puerta: string;
+            piso: string;
+            codigo_postal: string;
+            pais: string;
+            poblacion: {
+                nombre: string;
+                provincia: string;
+            } | null;
+        } | null;
     };
     productos: DetallePedido[];
 }
